2eb22021d1762ca689743aa520d857bb627ef7c1
Travis Lee <travisslee223@gmail.com>
2020-09-11 00:16:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   if array.length == 1 
     array
   elsif array.length == 2 
-  array.join(" and ")
-else array.length >= 3 
-  array.join()
-
+   array.join(" and ")
+  else array.length >= 3 
+    array[-1] = "and #{array[-1]}"
+    array.join(" , ")
+    end
 end
